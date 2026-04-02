ee19424bd02f74cba14ce0a643d2404d011c0612
Navya <manjilbbudhathoki@gmail.com>
2026-04-02 11:12:10 +0545

    Refactor the dir Strucutre

--- automations/stock_market/automate_dead_stock.py
+++ automations/stock_market/automate_dead_stock.py
@@ -2,8 +2,8 @@ import requests
 import json
 
 cookies = {
-    'XSRF-TOKEN': 'eyJpdiI6ImxTM1FrbFZKYzQ3REhGc2diYVFIeEE9PSIsInZhbHVlIjoiZ1FMQmJnaGZJSG5pWFpZeUZNb2NpM2twZ3M4NDZLM3ZuZ284cEVKZ3BNeFFKSTRRcEhPT3VtWWd1aHBtZ1BMdWlIMHlkTDdXNythcGJRZ3VaOGdQaUpZOG1vQnY1RmRhMkNLUWQ5SkpmMkx2d05NVlg4L2FMbGczb1NXZFhvSUEiLCJtYWMiOiI0YmIxMjliZTcyMWZiNjUzNjgwMWZkZDA2NzQ5YTM0MjkzYTJkZDE0NjJiODNiM2RmMTBiYWE1Y2RmOTNiYmQxIn0%3D',
-    'sharesansar_session': 'eyJpdiI6ImZuNG9DWVN4b0o0RW5KN2ZpdE1sSlE9PSIsInZhbHVlIjoiK1E5Mjl5V3FvdTlRT1FreVBtcDZ5aEVXSElkMEdnZUJTN2tEdjFJTjhXN0hjV2FrazBZSTUvODBRL3NIUEZsdDh0VllIVHAxYitRbDE4M1RCdTRKZGgvOHMyT0lVNUxWaTUyc084Qjh1NHEwOGVGSGJBWmdCVlB6alVocXF3RGIiLCJtYWMiOiIzOWE4Mjc3Mjc0M2Y5YTA0YjJmOTRlYjY3ZTRjODRkYzBhMDkzMWYwNjExNmQxMDRjYmQ2MjUyMzBkMWEwOWQzIn0%3D',
+    'XSRF-TOKEN': 'eyJpdiI6Ikp0SDRycFgzZ2JsYjFiaDJZbDlWTGc9PSIsInZhbHVlIjoiT3VXWm9TZG5XWko1Y2JmMkVQUHJFblhoRjRoci9LMVVwbXU3MUMxcFlPSHo4VVJaZ3RkdlQ2TDNmRXJJU0tLZDVJMFdOZzMyeDZ0VDVrNXk5Q0ZaNHgrWUZVSy9LdDhnZTFKOGFVREpCbDZsYlQ0aERjdzZ3VTU1Q1lveThCMkYiLCJtYWMiOiJiNWI1OTRlZjdmNzc0N2Q4NGMyYTEyNDYyOTEzODRjYzlkMWNkNjEyOTViN2FhNTIzOTA2MmQxNzE2MGFjNTE2In0=',
+    'sharesansar_session': 'eyJpdiI6IlFqV1FqTjRoa1dQRWVhTE9laDQ0Qmc9PSIsInZhbHVlIjoiNFczQW45bisrUWdJV3JjZUQ3MjgwbUdOcFgwQTFUdjVEOFM1dGJ5THZNVXNjb0E1QWJlek1mZWtBazFGUlhRanpqVjhkZkZ5b0Y1cm9QU1pBMXJkV01ScGxNRjIzN2owL1d5NTJNaUVpWmxvVzVOOUszM0dzQWZuUHQ1M0RoNGMiLCJtYWMiOiI0YzkyN2M5OTUxYTc4ZjJlMmYxMGJhYWRhMWU3YjI2YTY5NzdhZThhNWJkZWJlYTYxYzNlMDY5ZDQ5OTI1YjNmIn0=',
 }
 
 headers = {
@@ -13,7 +13,7 @@ headers = {
     'origin': 'https://www.sharesansar.com',
     'referer': 'https://www.sharesansar.com/company/ARDBL',
     'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/146.0.0.0 Safari/537.36',
-    'x-csrf-token': 'OMzlHk61iUunYvO59J3JewK0qMOMDKNhJlNRmtQh',
+    'x-csrf-token': 'jRAF6VSBuzBE1hSPLHP039FIGzK3LlZ64gwBxZF7',
     'x-requested-with': 'XMLHttpRequest',
 }
 
@@ -60,31 +60,12 @@ def fetch_all_price_history(company_id, length=50):
 
 # Dictionary of companies: { "Company Name": "ID" }
 companies = {
-    "KKBL": "411",
-    "KMBL": "327",
-    "METRO": "334",
-    "MIDBL": "310",
-    "MMDBL": "349",
-    "MSBBL": "321",
-    "NBSL": "416",
-    "NDFL": "57",
-    "NEFL": "84",
-    "NGBL": "190",
-    "RBSL": "187",
-    "RHPC": "468",
-    "SAFL": "567",
-    "SDBL": "180",
-    "SETI": "45",
-    "SEWA": "189",
-    "SKDBL": "479",
-    "SMBF": "79",
-    "WOMI": "425",
-    "WDBL": "292",
-    "UFIL": "93",
-    "TNBL": "291",
-    "TBBL": "169",
-    "TDBL": "338",
-    "SUBBL": "184"
+    "NIDC": "156",
+    "HATH": "46",
+    "KEBL": "154",
+    "JBNL": "331",
+    "JEFL": "326",
+    "GDBL.csv": "163"
 }
 
 # Fetch and save data for each company
